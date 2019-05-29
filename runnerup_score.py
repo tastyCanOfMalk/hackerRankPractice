fd494b8c82e970d9038226aa50b3cafbbc5b7214
Eddie <tastyCanOfMalk@users.noreply.github.com>
2019-05-29 12:06:32 -0400

    use set instead of unique

--- runnerup_score.py
+++ runnerup_score.py
@@ -1,11 +1,17 @@
 # https://www.hackerrank.com/challenges/find-second-maximum-number-in-a-list/problem?h_r=next-challenge&h_v=zen
 
-import numpy
+# using numpy.unique
+# import numpy
+# n = int(input())
+# x = list(map(int, input().split()))
+# x.sort()
+# x = numpy.unique(x)
+# if 2 <= n <= 10 and all(-100 <= i <= 100 for i in x):
+#     print(x[-2])
 
+# without numpy
 n = int(input())
-x = list(map(int, input().split()))
+x = list(set(map(int, input().split())))
 x.sort()
-x = numpy.unique(x)
-
 if 2 <= n <= 10 and all(-100 <= i <= 100 for i in x):
     print(x[-2])
