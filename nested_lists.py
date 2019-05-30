273fae6a04ebd91baf7950e0c62e3d192d5584ac
Eddie <tastyCanOfMalk@users.noreply.github.com>
2019-05-30 19:13:16 -0400

    add solution from discussion

--- nested_lists.py
+++ nested_lists.py
@@ -58,3 +58,12 @@ else:
     newlist.sort()
     for x in range(len(newlist)):
         print(newlist[x])
+
+
+# solution from discussion
+# marksheet = []
+# for _ in range(0,int(input())):
+#     marksheet.append([input(), float(input())])
+
+# second_highest = sorted(list(set([marks for name, marks in marksheet])))[1]
+# print('\n'.join([a for a,b in sorted(marksheet) if b == second_highest]))
