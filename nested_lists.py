0bb4989b9d8e6363fd5ce30743fe17452abfce72
Eddie <tastyCanOfMalk@users.noreply.github.com>
2019-05-31 09:15:36 -0400

    cleanup

--- nested_lists.py
+++ nested_lists.py
@@ -1,36 +1,44 @@
 # https://www.hackerrank.com/challenges/nested-list/problem?h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
 
 # get n, create empty list
-# n = int(input())
-n=5
+n = int(input())
 arr = [[0 for _ in range(2)] for _ in range(n)]
 
-arr[0][0]= "Harsh"
-arr[0][1]= 20
-arr[1][0]= "Beria"
-arr[1][1]= 20
-arr[2][0]= "Varun"
-arr[2][1]= 19
-arr[3][0]= "Kakunami"
-arr[3][1]= 19
-arr[4][0]= "Vikas"
-arr[4][1]= 21
-
-
+# test inputs
+# arr[0][0]= "sona"
+# arr[0][1]= -25.001
+# arr[1][0]= "mona"
+# arr[1][1]= -25.0001
+# arr[2][0]= "mini"
+# arr[2][1]= -25.000
+# arr[3][0]= "rita "
+# arr[3][1]= -25.0
+# arr[4][0]= "Vikas"
+# arr[4][1]= 21
 
 # loop to get inputs
-# count = 0
-# while count < n and 2 <= n <= 5: 
-#     arr[count][0] = input()
-#     arr[count][1] = input()
-#     count+=1
+count = 0
+while count < n and 2 <= n <= 5: 
+    arr[count][0] = input()
+    arr[count][1] = input()
+    count+=1
 
 # sort ascending
 arr.sort(key=lambda x: x[1])
 
-# need to loop or remove all duplicate minimum values
-# remove min value
+# remove min value(s)
+removes = []
+min1 = arr[0]
 arr.remove(arr[0])
+
+# find duplicate min values, store index
+for x in range(len(arr)):
+    if min1[1] == arr[x][1]:
+        removes.append(x)
+  
+# remove values stores in index
+for y in range(len(removes)):
+    arr.remove(arr[removes[0]])
 
 # store second min, then remove it
 min2 = arr[0]
@@ -38,7 +46,7 @@ arr.remove(arr[0])
 
 # store matching values
 store=[]
-for x in range(n-2):
+for x in range(n - (len(removes)+2)):
     if min2[1] == arr[x][1]:
         store.append(arr[x])
     
